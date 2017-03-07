e64efe7c2170c7e6a33483b9df2ad33068a67c24
denimar <denimar@gmail.com>
2017-03-07 11:34:57 -0300

    implementing gh-pages

--- webpack.config.js
+++ webpack.config.js
@@ -56,7 +56,7 @@ module.exports = {
         test: /\.(jpe?g|png|gif|svg)$/i,
         loaders: [
           'file?hash=sha512&digest=hex&name=[hash].[ext]',
-          'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+          'image-webpack'
         ]
       }
     ],
