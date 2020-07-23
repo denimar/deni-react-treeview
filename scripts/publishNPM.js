83eb79d150d13b3ca039753238165f671a229fbd
denimar <denima@gmail.com>
2020-07-23 03:12:27 -0300

    partial commit

--- scripts/publishNPM.js
+++ scripts/publishNPM.js
@@ -1,8 +1,8 @@
-export function execShellCommand(cmd) {
+function execShellCommand(cmd) {
   const exec = require('child_process').exec
   return new Promise((resolve, reject) =>
     exec(cmd, (error, stdout, stderr) =>
-      error ? reject(new Error('Folder is Empty')) : resolve(stdout ? stdout : stderr)
+      error ? reject(new Error(error.message)) : resolve(stdout ? stdout : stderr)
     )
   )
 }
