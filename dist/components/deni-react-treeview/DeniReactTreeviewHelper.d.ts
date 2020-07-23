c48cd0d4b8f890d2d5fd040010d52bed9c793968
denimar <denima@gmail.com>
2020-07-23 03:24:10 -0300

    partial commit

--- dist/components/deni-react-treeview/DeniReactTreeviewHelper.d.ts
+++
@@ -1,5 +0,0 @@
-declare const loadingSetup: (treeview: any) => void;
-declare const setTheme: (treeview: any, newTheme: any) => void;
-declare const load: (treeview: any, item: any) => Promise<unknown>;
-declare const loadData: (treeview: any, data: any, item: any) => void;
-export { loadingSetup, setTheme, load, loadData };
--- scripts/publishNPM.sh
+++ scripts/publishNPM.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 npm version patch
 yarn build
+pwd
 cd dist
+pwd
 npm pack
 npm publish
