0d8d3d768c3eecccb82d4f25eb15cb5865fce44b
denimar <denima@gmail.com>
2020-07-23 03:00:44 -0300

    partial commit

--- scripts/publishNPM.js
+++ scripts/publishNPM.js
@@ -4,15 +4,11 @@ function execute(command) {
   const exec = require('child_process').exec
 
   exec(command, (err, stdout, stderr) => {
+    if (err) console.log(`Error publishing NPM: ${err.message}`)
     process.stdout.write(stdout)
   })
 }
 
 (async () => {
-  execute('npm version patch && yarn build && cd dist && pwd')
-  execute('npm pack')
-  execute('npm publish')
-  execute('pwd')
+  execute('npm version patch && yarn build && cd dist && npm pack && npm publish')
 })();
-
- //&&  && cd dist && npm pack && npm publish && cd ..
