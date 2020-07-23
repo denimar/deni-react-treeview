b2b9045ab76b0233a95930d1055a422e43b36bf1
denimar <denima@gmail.com>
2020-07-23 03:18:11 -0300

    partial commit

--- scripts/publishNPM.js
+++ scripts/publishNPM.js
@@ -2,7 +2,7 @@ function execShellCommand(cmd) {
   const exec = require('child_process').exec
   return new Promise((resolve, reject) =>
     exec(cmd, (error, stdout, stderr) =>
-      error ? reject(new Error(error.message)) : resolve(stdout ? stdout : stderr)
+      error || stderr ? reject(error || stderr) : resolve(stdout)
     )
   )
 }
